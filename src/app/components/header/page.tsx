8825aac14d94e31e994b4cd3417dc3871a185362
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-17 11:03:37 +0700

    add mobile nav

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export default function Header() {
   const NavLink = [
@@ -21,6 +24,8 @@ export default function Header() {
     },
   ];
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="flex justify-between items-center px-3 py-3 md:px-10 mx-auto max-w-7xl">
       <Link href="/">
@@ -29,6 +34,7 @@ export default function Header() {
         </h1>
       </Link>
       {/* Nav Desktop */}
+
       <ul className="sm:flex gap-6 hidden">
         {NavLink.map(({ menu, link }, index) => (
           <li key={index}>
@@ -36,6 +42,20 @@ export default function Header() {
           </li>
         ))}
       </ul>
+
+      <div className="relative sm:hidden">
+        {isOpen ? <FaTimes onClick={() => setIsOpen((prev) => !prev)} /> : <FaBars onClick={() => setIsOpen((prev) => !prev)} className="md:hidden" />}
+        {/* Nav Mobile */}
+        {isOpen && (
+          <ul className="sm:hidden space-y-4 absolute right-8 top-0 bg-gray-900 z-50">
+            {NavLink.map(({ menu, link }, index) => (
+              <li key={index}>
+                <Link href={link}>{menu}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </nav>
   );
 }
