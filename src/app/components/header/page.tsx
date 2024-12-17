957ab15c6f909e157a828a7d73eecc9972c65d26
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-17 10:14:14 +0700

    migrated from other repo

---
+++ src/app/components/header/page.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  const NavLink = [
+    {
+      menu: 'Beranda',
+      link: '/',
+    },
+    {
+      menu: 'Artikel',
+      link: '/artikel',
+    },
+    {
+      menu: 'Kosakata',
+      link: '/kosakata',
+    },
+    {
+      menu: 'Kontak',
+      link: '/kontak',
+    },
+  ];
+
+  return (
+    <nav className="flex justify-between items-center px-3 py-3 md:px-10 mx-auto max-w-7xl">
+      <Link href="/">
+        <h1 className="font-semibold text-xl sm:text-2xl">
+          <span>&#128218;</span> BahasaArUp
+        </h1>
+      </Link>
+      {/* Nav Desktop */}
+      <ul className="sm:flex gap-6 hidden">
+        {NavLink.map(({ menu, link }, index) => (
+          <li key={index}>
+            <Link href={link}>{menu}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
---
+++ src/app/lib/data.ts
@@ -0,0 +1,32 @@
+export const wordOptions = [
+  {
+    text: 'rumah',
+    value: 'بَيْتٌ',
+    image: '/home.jpg',
+  },
+  {
+    text: 'buku',
+    value: 'كِتَابٌ',
+    image: '/book.jpg',
+  },
+  {
+    text: 'masjid',
+    value: 'مَسْجِدٌ',
+    image: '/mosque.jpg',
+  },
+  {
+    text: 'meja',
+    value: 'مَكْتَبٌ',
+    image: '/table.jpg',
+  },
+  {
+    text: 'kursi',
+    value: 'كُرْسِيٌّ',
+    image: '/chair.jpg',
+  },
+  {
+    text: 'tempat tidur',
+    value: 'سَرِيْرٌ',
+    image: '/bed.jpg',
+  },
+];
