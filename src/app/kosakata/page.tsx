a56ff61c9af92f9b8a02d68fb1f08f29a0d6bd5c
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-20 11:03:53 +0700

    change folder structure

--- src/app/kosakata/page.tsx
+++ src/app/kosakata/page.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { wordOptions } from '../lib/data';
-import Image from 'next/image';
+import { useState } from "react";
+import { wordOptions } from "../lib/data";
+import Image from "next/image";
 
 type WordOption = {
   text: string;
@@ -13,17 +13,24 @@ type WordOption = {
 export default function Home() {
   const [selectedOption, setSelectedOption] = useState<WordOption | null>(null);
 
-  const onChange: React.ComponentProps<'select'>['onChange'] = (e) => {
+  const onChange: React.ComponentProps<"select">["onChange"] = (e) => {
     const selectedValue = e.target.value;
-    const option = wordOptions.find((opt) => opt.value === selectedValue) || null;
+    const option =
+      wordOptions.find((opt) => opt.value === selectedValue) || null;
     setSelectedOption(option);
   };
 
   return (
-    <main className="mx-auto flex flex-col items-center h-screen justify-center md:px-10 p-4 max-w-7xl">
-      <h1 className="block bg-clip-text text-transparent font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center pb-8 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">Apa Bahasa Arabnya...</h1>
-      <div className="pb-8 lg:w-1/3 w-2/3">
-        <select className="bg-gray-800 border w-full border-gray-700 p-4 shadow-sm  outline-none rounded-lg" onChange={onChange} value={selectedOption?.value || ''}>
+    <main className="mx-auto flex h-screen max-w-7xl flex-col items-center justify-center p-4 md:px-10">
+      <h1 className="block bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text pb-8 text-center text-4xl font-extrabold tracking-tight text-transparent sm:text-5xl lg:text-6xl">
+        Apa Bahasa Arabnya...
+      </h1>
+      <div className="w-2/3 pb-8 lg:w-1/3">
+        <select
+          className="w-full rounded-lg border border-gray-700 bg-gray-800 p-4 shadow-sm outline-none"
+          onChange={onChange}
+          value={selectedOption?.value || ""}
+        >
           <option value="" disabled>
             Pilih kata
           </option>
@@ -36,9 +43,21 @@ export default function Home() {
       </div>
       {/* Card */}
       {selectedOption && (
-        <div className="bg-gray-800 overflow-hidden rounded-lg border border-gray-700 p-4 shadow-sm transition hover:shadow-primary cursor-default">
-          {selectedOption?.image ? <Image className="aspect-[4/3] rounded-lg object-cover object-center" src={selectedOption.image} width={200} height={200} alt="gambar" /> : ''}
-          <p className="text-center pt-6 pb-4 text-4xl font-bold">{selectedOption?.value}</p>
+        <div className="hover:shadow-primary cursor-default overflow-hidden rounded-lg border border-gray-700 bg-gray-800 p-4 shadow-sm transition">
+          {selectedOption?.image ? (
+            <Image
+              className="aspect-[4/3] rounded-lg object-cover object-center"
+              src={selectedOption.image}
+              width={200}
+              height={200}
+              alt="gambar"
+            />
+          ) : (
+            ""
+          )}
+          <p className="pb-4 pt-6 text-center text-4xl font-bold">
+            {selectedOption?.value}
+          </p>
         </div>
       )}
     </main>
